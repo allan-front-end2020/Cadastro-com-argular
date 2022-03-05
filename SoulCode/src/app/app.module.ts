7e11732b43cd41850e02857b981a36cb4ed258a0
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-03-05 18:17:21 -0300

    cadastro angular finalizado

--- SoulCode/src/app/app.module.ts
+++ SoulCode/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { InputNameComponent } from './input-name/input-name.component';
+import { InputEmailComponent } from './input-email/input-email.component';
+import { InputTelComponent } from './input-tel/input-tel.component';
+import { InputEstadoComponent } from './input-estado/input-estado.component';
+import { BtnComponent } from './btn/btn.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    InputNameComponent,
+    InputEmailComponent,
+    InputTelComponent,
+    InputEstadoComponent,
+    BtnComponent,
+    
   ],
   imports: [
     BrowserModule
---
+++ SoulCode/src/app/input-estado/input-estado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-estado',
+  templateUrl: './input-estado.component.html',
+  styleUrls: ['./input-estado.component.css']
+})
+export class InputEstadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
